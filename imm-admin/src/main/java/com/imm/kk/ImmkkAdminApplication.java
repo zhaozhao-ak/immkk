a4a92f278fd4c638f28bfa6f00399bfc6f0efaeb
460644290@qq.com <zhao11810953>
2019-10-05 13:27:55 +0800

    fix 优化

--- imm-admin/src/main/java/com/imm/kk/ImmkkAdminApplication.java
+++ imm-admin/src/main/java/com/imm/kk/ImmkkAdminApplication.java
@@ -2,7 +2,6 @@ package com.imm.kk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author imm-sz
--- imm-setting/src/main/java/com/imm/kk/mapper/setting/HdisAgeMapper.java
+++ imm-setting/src/main/java/com/imm/kk/mapper/setting/HdisAgeMapper.java
@@ -3,7 +3,6 @@ package com.imm.kk.mapper.setting;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.imm.kk.entity.setting.HdisAge;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -11,12 +10,13 @@ import java.util.List;
  * @date 2019-05-13 19:39:21
  */
 @Mapper
-@Component
 public interface HdisAgeMapper extends BaseMapper<HdisAge> {
 
 
     List<HdisAge> selectAllData();
 
     int selectCnt();
+
+    HdisAge selectInfo();
 
 }
